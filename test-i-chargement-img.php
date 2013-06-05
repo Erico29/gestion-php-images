d67361d351bc558095d2354bd044128a08f29ddf
Erico29 <contact@studio-tranchart.com>
2013-06-05 12:04:52 +0200

    Engregistrement + début de contrôle type de fichier
    Enregistrement des images uploadées dans un dossier spécifique.
    + Un début de contrôle sur le type de fichier

--- test-i-chargement-img.php
+++ test-i-chargement-img.php
@@ -1,4 +1,5 @@
 <?php
+// MESSAGES D'ERREUR DE CHARGEMENT
 $message = "";
 $message .= '<p><strong>File name :</strong> ' . $_FILES['userfile']['name'] . ' <em>//Le nom original du fichier, comme sur le disque du visiteur (exemple : mon_icone.png).</em></p>';
 $message .= '<p><strong>File type :</strong> ' . $_FILES['userfile']['type'] . ' <em>//Le type du fichier. Par exemple, cela peut être « image/png ».</em></p>';
@@ -9,16 +10,43 @@ $message .= '<p><strong>File error :</strong> ' . $_FILES['userfile']['error'] .
 $message .= '<p><strong>Titre :</strong> ' . $_POST['titre'] . ' <em>//Titre récupéré.</em></p>';
 $message .= '<p><strong>Description :</strong> ' . $_POST['description'] . ' <em>//Description récupérée.</em></p>';
 
-// Message d'erreur éventuelle
+// AFFICHAGE
 $message_erreur = "";
-$taille_max = 51200; // 50 octets
-if ($_FILES['userfile']['size'] > $taille_max) 
-    $message_erreur .= "Le fichier est trop lourd (50 ko maximum)<br/>";
+$poids_max = 51200; // Poids max autorisée : 50 octets
+if ($_FILES['userfile']['size'] > $poids_max)
+    $message_erreur .= "<p>Le fichier est trop lourd (50 ko maximum)</p><br/>";
 
 if ($_FILES['userfile']['error'])
-    $message_erreur .= "Erreur(s) de fichier : ".$_FILES['userfile']['error'];
+    $message_erreur .= "<p>Erreur(s) de fichier : " . $_FILES['userfile']['error'] . "</p><br/>";
 // On peut également vérifier l'extension et la taille maximum du fichier 
 // Lire : http://www.siteduzero.com/informatique/tutoriels/upload-de-fichiers-par-formulaire/recuperer-le-fichier
+// ENREGISTREMENT DU FICHIER
+// Emplacement du dossier pour enregistrer les fichiers :
+// $dossier = $_SERVER['DOCUMENT_ROOT'].'Sites/tests/gestion-php-images/images_telechargees';
+// Nom du fichier
+
+// On vérifie si l'image est bien de type image/jpeg. Sinon, on ne gère pas l'extension
+if ($_FILES['userfile']['type'] = "image/jpeg")
+        $extension = ".jpg";
+else
+    $extension = "";
+    
+$nom = $_POST['titre'].$extension;
+$dossier_upload = "images_telechargees";
+$resultat = move_uploaded_file($_FILES['userfile']['tmp_name'], $dossier_upload.'/'.$nom);
+if ($resultat) $message .= "<p><strong>Transfert réussi</strong></p><br/>";
+else $message_erreur .= "<p>Problème de transfert du fichier</p>";
+
+
+
+//$dossier = $_SERVER['DOCUMENT_ROOT'].'images/avatar';
+//			chmod($dossier,0777);
+//			 $fichier = basename($_FILES['avatar']['name']);
+//			 if(!move_uploaded_file($_FILES['avatar']['tmp_name'], $dossier.$fichier)){
+//				echo "pb";
+//			 }else{
+//				echo "ok";
+//			 }
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -48,14 +76,14 @@ if ($_FILES['userfile']['error'])
         <br/>
         <div style="border: 1px solid red; padding: 10px;">
             <h2>Retour valeurs :</h2>
-            <?php
-            echo $message;
-            ?>
+<?php
+echo $message;
+?>
         </div>
         <br/>
         <br/>
         <div style="border: 1px solid red; padding: 10px;">
-            <h2>Message d'erreur éventuelle</h2>
+            <h2>Message éventuel</h2>
             <?php
             echo $message_erreur;
             ?>
